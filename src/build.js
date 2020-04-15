c1db24312808cbfac01f2a64b599dc6bc32fc5da
Phil Sturgeon <me@philsturgeon.uk>
2020-04-15 13:39:58 +0100

    dates

--- src/build.js
+++ src/build.js
@@ -9,12 +9,19 @@ const publicFile = path.join(__dirname, `../public/index.html`)
 const eventsData = yaml.load(fs.readFileSync(eventsFile, 'utf8'));
 const templateHtml = fs.readFileSync(templateFile,'utf8');
 
-eventsList = eventsData.map(event => `
+eventsList = eventsData.map(event => {
+  const d = event.date;
+  const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
+  const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d)
+  const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+
+  return `
 <li class="${event.climate}">
   <h3><a href="${event.url}">${event.title}</a></h3>
-  <div>${event.about}</div>
+  <span>${mo} ${da}, ${ye}</span> - ${event.about}
 </li>
-`);
+`
+});
 
 let newHtml = templateHtml;
 
